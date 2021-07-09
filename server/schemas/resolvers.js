8bf39fa27595d95ab40328ae6c0434fc476728a3
iiTONELOC <anthonytropeano@me.com>
2021-07-09 16:15:26 -0400

    added queries for finding all and single user

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,8 +1,19 @@
+const { User, bookSchema } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
 const resolvers = {
     Query: {
-        helloWorld: () => {
-            return 'Hello world!';
-        }
+        users: async () => {
+            return User.find()
+                .select('-__v -password')
+                .populate('bookSchema')
+        },
+        user: async (parent, { username }) => {
+            return User.findOne({ username })
+                .select('-__v -password')
+                .populate('friends')
+                .populate('thoughts');
+        },
     }
 };
 
